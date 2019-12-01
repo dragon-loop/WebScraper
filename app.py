2d3cc03069525f416ad30eeb7db081b70efb6604
vangoespow <chris.van67@gmail.com>
2019-12-01 14:10:45 -0500

    SDDL-104: Added custom shape rendering to JSON

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import requests
 import urllib.request
 from bs4 import BeautifulSoup
+from collections import OrderedDict
+import json
 
 DragonRouteUrl = "https://drexel.edu/facilities/transportation/busServiceSchedules/DragonRoute/"
 PoweltonSpringGardenRouteUrl = "https://drexel.edu/facilities/transportation/busServiceSchedules/PoweltonSpringGardenRoute/"
@@ -12,17 +14,34 @@ def pushToDb(data):
 def comparison(data):
     print()
 
-def constructShape(data):
-    print()
+def constructShape(data, name):
+    with open(name + '.json','w') as outfile:
+        json.dump(data, outfile)
 
-def getData(url):
+def getData(url, name):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
-    allTrTags = soup.findAll('tr')
-    constructShape(allTrTags)
+    table = soup.find("table")
+    rows = soup.find_all("tr")
+    headerTr = table.find("tr")
+    columnNames = headerTr.find_all("strong")
 
+    #create list of column headers
+    headers={}
+    for i in range(len(columnNames)):
+        headers[i] = columnNames[i].text.strip().lower()
+
+    #append data to headers
+    data = []
+    for row in rows:
+        cells = row.find_all("td")
+        items = {}
+        for index in headers:
+            items[headers[index]] = cells[index].text
+        data.append(items)
+    constructShape(data, name)
 if __name__ == "__main__":
-    getData(DragonRouteUrl)
+    getData(DragonRouteUrl, "DragonRoute")
     #getData(PoweltonSpringGardenRouteUrl)
     #getData(QueenLaneRouteUrl)
 
