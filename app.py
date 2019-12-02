b6a83d8e2ca8a897a39f03fdccf7bc2a11127d6d
vangoespow <chris.van67@gmail.com>
2019-12-02 18:53:26 -0500

    SDDL-104: Add new DB function

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 import requests
 import urllib.request
 from bs4 import BeautifulSoup
-from collections import OrderedDict
 import json
 from os import path
 import os.path
 import sys
 import psycopg2
-from config import config
 
 DragonRouteUrl = "https://drexel.edu/facilities/transportation/busServiceSchedules/DragonRoute/"
 PoweltonSpringGardenRouteUrl = "https://drexel.edu/facilities/transportation/busServiceSchedules/PoweltonSpringGardenRoute/"
 QueenLaneRouteUrl = "https://drexel.edu/facilities/transportation/busServiceSchedules/QueenLaneRoute/"
 
 def pushToDb(data, name):
-    print()
+    #localhost db -> currently wont connect
+    #conn = psycopg2.connect("host='localhost' dbname='postgres' port='60561' user='postgres' password='password' connect_timeout=60")
+    conn = psycopg2.connect(host="localhost", database="postgres", user="postgres", password="password", port="60690")
+    cur = conn.cursor()
 
 def comparison(data, name):
     #open the file for compare
@@ -23,8 +24,11 @@ def comparison(data, name):
     compareAgainstJsonObj = json.loads(data)
 
     a, b = json.dumps(originalJsonObj), json.dumps(compareAgainstJsonObj)
+    #if not equal then push to db else dont do anything and
     if not a == b:
         pushToDb(data, name)
+    else:
+        return
 
 def constructShape(data, name):
     if not path.exists(name + '.json'):
@@ -57,7 +61,8 @@ def getData(url, name):
         data.append(items)
     constructShape(data, name)
 if __name__ == "__main__":
-    getData(DragonRouteUrl, "DragonRoute")
+    pushToDb('test', "DragonRoute")
+    #getData(DragonRouteUrl, "DragonRoute")
     #getData(PoweltonSpringGardenRouteUrl, "PoweltonSpringGardenRoute")
     #getData(QueenLaneRouteUrl, "QueenLaneRoute")
 
